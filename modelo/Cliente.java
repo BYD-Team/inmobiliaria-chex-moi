43d90e36c5caa0293e3436aca32ff45a8ae5f56c
Yael2004 <yaelalfredosalazaraguilar14@gmail.com>
2024-05-20 14:59:43 -0600

    feat: las carpetas se renombraron

---
+++ modelo/Cliente.java
@@ -0,0 +1,54 @@
+package modelo;
+
+public class Cliente {
+    private int idCliente;
+    private String nombre;
+    private String apellidoPaterno;
+    private String correoElectronico;
+    private String clave;
+
+    public Cliente() {
+
+    }
+
+    public int getIdCliente() {
+        return idCliente;
+    }
+    
+    public String getNombre() {
+        return nombre;
+    }
+
+    public String getApellidoPaterno() {
+        return apellidoPaterno;
+    }
+    
+    public String getCorreoElectronico() {
+        return correoElectronico;
+    }
+    
+    public String getClave() {
+        return clave;
+    }
+    
+    public void setIdCliente(int idCliente) {
+        this.idCliente = idCliente;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public void setApellidoPaterno(String apellidoPaterno) {
+        this.apellidoPaterno = apellidoPaterno;
+    }
+
+    public void setCorreoElectronico(String correoElectronico) {
+        this.correoElectronico = correoElectronico;
+    }
+
+
+    public void setClave(String clave) {
+        this.clave = clave;
+    }
+}
