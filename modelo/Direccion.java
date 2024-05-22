856def23ba9eee3b6b1d3f3bda0063fa222b1769
xxeris19 <dylanoficial2003@gmail.com>
2024-05-22 16:47:51 -0600

    fix: base de datos modificada

---
+++ modelo/Direccion.java
@@ -0,0 +1,9 @@
+package modelo;
+
+public class Direccion {
+    private String calle = "";
+    private String colonia = "";
+    private int codigoPostal = 0;
+    private String numeroExterior = "";
+    private String numeroInterior = ""; 
+}
---
+++ modelo/Propiedad.java
@@ -0,0 +1,12 @@
+package modelo; 
+
+public class Propiedad {
+    private float precio; 
+    private String nombre = "";
+    private String operación = ""; 
+    private float dimensiones = 0; 
+    private Direccion direccion;
+    private int habitaciones = 0; 
+    private String patio = ""; 
+    private int idPropiedad = 0;
+}
