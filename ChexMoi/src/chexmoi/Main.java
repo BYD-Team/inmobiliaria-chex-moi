bab70a63b949e2dc7df60321e34fb2831e2e054b
brianlewyn <brianlewyn@gmail.com>
2024-05-23 03:48:55 -0600

    feat: habilitar el inicio de sesion desde la clase Main

--- ChexMoi/src/chexmoi/Main.java
+++ ChexMoi/src/chexmoi/Main.java
@@ -1,5 +1,26 @@
-package chexmoi; 
+package chexmoi;
 
-public class Main {
-    
+import java.io.IOException;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class Main extends Application {
+
+    @Override
+    public void start(Stage primaryStage) throws IOException {
+        Parent root = FXMLLoader.load(getClass().getResource("vista/InicioDeSesion.fxml"));
+
+        primaryStage.setTitle("Inicio de Sesión");
+        primaryStage.setScene(new Scene(root));
+        primaryStage.show();
+    }
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+
 }
---
+++ ChexMoi/src/chexmoi/controlador/InicioDeSesionControlador.java
@@ -0,0 +1,25 @@
+package chexmoi.controlador;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
+
+public class InicioDeSesionControlador {
+
+    @FXML
+    private Button iniciarSesionButton;
+
+    @FXML
+    private TextField mailTextField;
+
+    @FXML
+    private PasswordField passwordPasswordField;
+
+    @FXML
+    void iniciarSesion(ActionEvent event) {
+
+    }
+
+}
